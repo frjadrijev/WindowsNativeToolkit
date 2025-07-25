3447189a90169abfc854b892f59ba413c7e4a6c3
frjadrijev <frjadrijev@outlook.com>
2025-07-26 00:21:36 +0200

    Created fresh and buildable WindowsNativeToolkit.vcxproj from WinUI Blank App (Packaged) template, removed stale or onused code

--- WindowsNativeToolkit/pch.h
+++ WindowsNativeToolkit/pch.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <windows.h>
 #include <unknwn.h>
 #include <restrictederrorinfo.h>
@@ -9,17 +8,9 @@
 // conflict with Storyboard::GetCurrentTime
 #undef GetCurrentTime
 
-#include <winrt/base.h>
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.ApplicationModel.Activation.h>
-#include <winrt/Windows.UI.Core.h>
-#include <winrt/Windows.UI.Xaml.h>
-#include <winrt/Windows.UI.Xaml.Controls.h>
-#include <winrt/Windows.UI.Xaml.Data.h>
-#include <winrt/Windows.UI.Xaml.Interop.h>
-#include <winrt/Windows.UI.Xaml.Markup.h>
-#include <winrt/Windows.UI.Xaml.Media.h>
 #include <winrt/Microsoft.UI.Composition.h>
 #include <winrt/Microsoft.UI.Xaml.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.h>
